cc6879d84547ada3ed44ade6de1133daea0ce8e0
ahmdmalik <91359950+ahmdmalik@users.noreply.github.com>
2021-10-11 23:58:50 +0700

    Latihan Struktu Kendali IF

---
+++ Struktut Kendali IF/LatihanIF.py
@@ -0,0 +1,10 @@
+#struktur kendali if
+pelanggan = "Budi Santoso"
+totalbelanja = 150000
+
+if(totalbelanja > 100000):
+    keterangan = "Selamat Anda Mendapat Hadiah"
+else:
+    keterangan = "Terima Kasih Sudah Berbelanja"
+
+print("pelanggan",pelanggan,"\nTotal Belanja Anda Rp.",totalbelanja,"\n",keterangan)
---
+++ Struktut Kendali IF/LatihanTernary.py
@@ -0,0 +1,17 @@
+#siswa dinyatakan lulus minimal 60 nilainya
+nama = "Budi Santoso"
+matpel = "Matematika"
+nilai = 59.99
+
+#siswa lain
+nama = "Malik Fajar"
+matpel = "Pemograman Web"
+nilai = 80
+
+#ternary
+keterangan = "lulus" if nilai >= 60 else "gagal"
+
+print("Nama Siswa \t:",nama,
+"\nMata Pelajaran \t:",matpel,
+"\nNilai \t\t:",nilai,
+"\nKeterangan \t:",keterangan,)
